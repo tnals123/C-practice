346e16df795a9bb44fbf74d1b82ec17af00f0c53
tnals123 <soo8608@naver.com>
2021-10-20 19:37:38 +0900

    update my practice

---
+++ 2.21/2.21/2.21.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main() {
+	cout << "   CCC   +       +   " << endl;
+	cout << "  C      +       +   " << endl;
+	cout << " C     +++++   +++++ " << endl;
+	cout << "  C      +       +   " << endl;
+	cout << "   CCC   +       +   " << endl;
+}
