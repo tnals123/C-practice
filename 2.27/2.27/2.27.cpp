c2be4a9cec846c63ef86e6512eefa16c420494ce
tnals123 <soo8608@naver.com>
2021-10-23 15:20:10 +0900

    update my practice

---
+++ 2.27/2.27/2.27.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main() {
+	cout << 'A' << endl;
+	cout << static_cast<int>('A') << endl;
+	cout << 'B' << endl;
+	cout << static_cast<int>('B') << endl;
+	cout << 'C' << endl;
+	cout << static_cast<int>('C') << endl;
+	cout << 'D' << endl;
+	cout << static_cast<int>('D') << endl;
+	cout << 'E' << endl;
+	cout << static_cast<int>('E') << endl;
+	cout << 'F' << endl;
+	cout << static_cast<int>('F') << endl;
+	cout << 'G' << endl;
+	cout << static_cast<int>('G') << endl;
+}
