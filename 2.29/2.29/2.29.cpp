9293c9838513726c50ae8f8520c5bd76906f2981
tnals123 <soo8608@naver.com>
2021-10-24 14:08:13 +0900

    my practice

---
+++ 2.29/2.29/2.29.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int i, a, b;
+int main() {
+	cout << "Face length of cube(cm)          Surface area of cube  (cm^2)        Volume of cube(cm^3)" << endl;
+	for (i = 0; i <= 4; i++) {
+		a = i * i * 6;
+		b = i * i * i;
+
+		cout << "           " << i << "                             " << a << "                                     " << b << endl;
+
+	}
+
+
+}
